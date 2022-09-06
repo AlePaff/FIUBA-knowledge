c6fb89a59e5f6ed44f1a07549d78dbf87e29c66e
AlePaff <43119891+AlePaff@users.noreply.github.com>
2022-09-05 23:37:55 -0300

    links a la wiki

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
-
+const loader = document.querySelector('#cargando_materias');
+const content = document.querySelector('#datos_materias');
 
 imprimirDatos();
 async function imprimirDatos(){
+
    const response = await fetch("materias.json");
    const datos = await response.json();
 
    // convertir los datos del JSON en variables para poder usarlas
    materias = "";
    for (let i = 0; i < datos.length; i++) {
-      materias += "<h5>"+datos[i].asignaturas+"</h5><a href=\""+datos[i].planificaciones+"\">link al pdf</a><br><pre>"+datos[i].contenido+"</pre><br><hr>";
+      codigo = "http://wiki.foros-fiuba.com.ar/materias:" + datos[i].asignaturas.slice(0,2) + ":" + datos[i].asignaturas.slice(3,5);
+      materias += "<h5>"+datos[i].asignaturas+"</h5><a href=\""+datos[i].planificaciones+"\">Link al PDF</a> | <a href=\""+codigo+"\">Wiki</a><br><pre>"+datos[i].contenido+"</pre><br><hr>";
    }
-      document.getElementById("datos").innerHTML = materias;
+ 
+   //get first and second character of a string
+   document.getElementById("datos_materias").innerHTML = materias;
+
+
+   
 }
 
 
