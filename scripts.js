5cbb3091f63807b1dbe4b8be8ccd240e9cb42386
AlePaff <alejandropaff@gmail.com>
2023-02-28 01:28:34 -0300

    loading icon and page icon

--- scripts.js
+++ scripts.js
@@ -1,24 +1,35 @@
-const loader = document.querySelector('#cargando_materias');
-const content = document.querySelector('#datos_materias');
+
 
 imprimirDatos();
-async function imprimirDatos(){
-
+async function imprimirDatos() {
+   // convertir los datos del JSON en variables para poder usarlas
    const response = await fetch("materias.json");
    const datos = await response.json();
 
-   // convertir los datos del JSON en variables para poder usarlas
-   materias = "";
+   var datos_materias_html = "";
+
    for (let i = 0; i < datos.length; i++) {
-      codigo = "http://wiki.foros-fiuba.com.ar/materias:" + datos[i].asignaturas.slice(0,2) + ":" + datos[i].asignaturas.slice(3,5);
-      materias += "<h5>"+datos[i].asignaturas+"</h5><a href=\""+datos[i].planificaciones+"\">Link al PDF</a> | <a href=\""+codigo+"\">Wiki</a><br><pre>"+datos[i].contenido+"</pre><br><hr>";
+      var materias = "";
+      var asignatura = datos[i].asignaturas;
+      var codigo = "http://wiki.foros-fiuba.com.ar/materias:" + asignatura.slice(0, 2) + ":" + asignatura.slice(3, 5);
+
+      materias += `
+      <div id="${asignatura}">
+         <h5>${asignatura}</h5>
+         <a href="${datos[i].planificaciones}" target="_blank">Link al PDF</a> | <a href="${codigo}" target="_blank">Wiki</a>
+         <br>
+         <pre>${datos[i].contenido}</pre>
+         <br>
+         <hr>
+      </div>
+      `;
+
+      datos_materias_html += materias;
    }
- 
-   //get first and second character of a string
-   document.getElementById("datos_materias").innerHTML = materias;
 
+   // mostrar los datos (TODO: Cambiarlo para que no se muestre todo de una)
+   document.getElementById("datos_materias").innerHTML = datos_materias_html;
 
-   
+   // ocultar el loader
+   document.getElementById("loader").style.display = "none";
 }
-
-
